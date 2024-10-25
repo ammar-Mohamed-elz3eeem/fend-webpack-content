c735f5c4ca5da828d819ad99b380077b2d84da12
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-25 11:37:18 +0300

    Add file loader to webpack to be able to process image import in js files

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import { handleSubmit } from './js/formHandler'
 import { checkForName } from './js/nameChecker'
+import rocket from './img/rocket.png'
 
 console.log("Hello, From index in client");
 alert("See it coming");
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,21 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/client/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader'
-      }
-    ]
-  }
-}
+	entry: "./src/client/index.js",
+	module: {
+		rules: [
+			{
+				test: /\.js$/i,
+				exclude: /(node_modules)/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				exclude: /(node_modules)/,
+        use: [
+          { loader: 'file-loader' }
+        ],
+			},
+		],
+	},
+};
